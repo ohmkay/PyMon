7f6d9e03402fad27fba34674cb9458f1d5acd39d
Caleb Sweeney <Caleb Sweeney>
2016-04-17 21:53:10 -0700

    Added support for different log file locations

--- test.py
+++ test.py
@@ -3,33 +3,50 @@ import os.path
 
 #Set file names
 logs_to_check = [
-"REGION1",
-"REGION5"
+	{'path':'./logs/', 'filename':'REGION1'},
+	{'path':'./logs/', 'filename':'REGION5'}
 ]
-
-#path to logs folder
-file_path = '../logs/'
 
 ################################
 #Add paths and date to filenames
 ################################
-def format_log_paths(logs_to_check, file_path):
-	logs_to_check = [file_path + log + "." + datetime.now().strftime("%m%d") for log in logs_to_check]
+def add_date_to_log(logs_to_check):
+	for log in logs_to_check:
+		for key, value in log.items():
+			if key == 'filename':
+				log[key] = value + "." + datetime.now().strftime("%m%d")
 	return logs_to_check
 
-logs_to_check = format_log_paths(logs_to_check, file_path)
+def create_full_path(logs_to_check):
+	for log in logs_to_check:
+		for key, value in log.items():
+			if key == 'path':
+				path = value
+			elif key == 'filename':
+				filename = value
+		log['full_path'] = path + filename
+	return logs_to_check
+	
 
-#Read errors in log files
+logs_to_check = add_date_to_log(logs_to_check)
+logs_to_check = create_full_path(logs_to_check)
+
+
+#iterates through logs looking for errors and writes them to a file
 lines = []
 for log in logs_to_check:
-	if os.path.isfile(log):
-		with open (log, 'r') as f:
-			for line in f:
-				if "error" in line:
-					lines.append(line.rstrip('\n'))
-	else:
-		print('File ' + log + ' does not exist.')
+	for key, value in log.items():
+		if key == 'full_path':
+			if os.path.isfile(value):
+				with open(value, 'r') as f:
+					for line in f:
+						if 'error' in line:
+							lines.append(line.rstrip('\n'))
+			else:
+				print('File ' + value + ' does not exist.')	
 
-#Testing
-for line in lines:
-	print(line)
+	#write data to output log file
+	for line in lines:
+		with open('./PyMonLog/test.txt', 'a') as f:
+			f.write(line+'\n')
+	lines = []
