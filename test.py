e6d935f9f6202b17029da712683bc2a7832db297
Caleb Sweeney <caleb.w.sweeney@gmail.com>
2016-04-23 17:03:02 -0700

    Added whitelist support

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ logs_to_check = [
 	{'path':'./logs/', 'filename':'REGION5'}
 ]
 
+#errors to ignore
+whitelist = [
+	"not an error",
+	"definitely...not an error"
+]
+
 ################################
 #Add paths and date to filenames
 ################################
@@ -26,22 +32,31 @@ def create_full_path(logs_to_check):
 				filename = value
 		log['full_path'] = path + filename
 	return logs_to_check
-	
+
+def check_against_whitelist(line):
+	for ignorable in whitelist:
+		if ignorable in line:
+			return True
+		else:
+			return False
 
 logs_to_check = add_date_to_log(logs_to_check)
 logs_to_check = create_full_path(logs_to_check)
 
+# test_string = 'this shouldnt be an error but you never know.  also, it\'s not ann error'
+# print(check_against_whitelist(test_string))
 
 #iterates through logs looking for errors and writes them to a file
 lines = []
 for log in logs_to_check:
 	for key, value in log.items():
 		if key == 'full_path':
-			if os.path.isfile(value):
+			if os.path.isfile(value):  #checks if log file exists
 				with open(value, 'r') as f:
 					for line in f:
 						if 'error' in line:
-							lines.append(line.rstrip('\n'))
+							if not check_against_whitelist(line):  #calls function to test error against whitelist
+								lines.append(line.rstrip('\n'))  #adds line and removes newline
 			else:
 				print('File ' + value + ' does not exist.')	
 
